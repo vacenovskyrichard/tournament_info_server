6e8d1af248ef17d8537e94e748fe6d1baba6fe7b
vacenovskyrichard <74417046+vacenovskyrichard@users.noreply.github.com>
2023-08-28 23:24:24 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,9 @@
 import os
 
-basedir = os.path.abspath(os.path.dirname(__file__))
 
 # postgres://tournament_info_user:Q0y9BOwD4CBzcWZCPRc0Vv3NLzuDT2gO@dpg-cjmflmnjbvhs73b95di0-a.frankfurt-postgres.render.com/tournament_info
-
-
 class ApplicationConfig:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
 
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    # SQLALCHEMY_BINDS = {"user_db": "sqlite:///" + os.path.join(basedir, "users.db")}
