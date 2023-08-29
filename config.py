ea8d59694351755708af3a5dd7b098cb32c74e22
vacenovskyrichard <74417046+vacenovskyrichard@users.noreply.github.com>
2023-08-29 12:55:26 +0200

    Update config.py

--- config.py
+++ config.py
@@ -6,4 +6,5 @@ class ApplicationConfig:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = "postgresql://tournament_info_user:Q0y9BOwD4CBzcWZCPRc0Vv3NLzuDT2gO@dpg-cjmflmnjbvhs73b95di0-a.frankfurt-postgres.render.com/tournament_info"
