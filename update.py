36d2be20a79ef7e367c4e4691b658c1b7ebf23c9
vacenric <vacenric@fit.cvut.cz>
2023-10-17 17:49:59 +0200

    Finish update script

--- update.py
+++ update.py
@@ -1,27 +1,16 @@
-# import json
-# from flask import Flask, jsonify, request, abort, session, Response
-# import os
-# import math
-# from flask import Flask, render_template, request, url_for, redirect
-# from Tournaments import TournamentManagement, TournamentInfo
-# from flask_marshmallow import Marshmallow
-# from flask_cors import CORS
+# from flask import Flask, jsonify
+# from flask import Flask
+# from Tournaments import TournamentManagement
 # from datetime import date, datetime
-# from datetime import datetime, timedelta, timezone
-# from flask_bcrypt import Bcrypt
-# from flask_mail import Mail, Message
-# from models import db, Tournament, User, get_uuid
+# from models import db, Tournament
 # from config import ApplicationConfig
+# from flask_marshmallow import Marshmallow
 
 # app = Flask(__name__)
 # app.config.from_object(ApplicationConfig)
 # db.init_app(app=app)
 
-# CORS(app)
-
 # ma = Marshmallow(app)
-# bcrypt = Bcrypt(app)
-
 # class TournamentSchema(ma.Schema):
 #     class Meta:
 #         fields = (
@@ -73,8 +62,6 @@
 #         db.session.add(tournament)
 #     db.session.commit()
 
-
-# @app.route("/update", methods=["GET"])
 # def update_database():
 #     tournaments = TournamentManagement()
 #     found_tournaments = tournaments.run_all_scrapers()
@@ -100,4 +87,73 @@
 #     results = tournaments_schema.dump(all_tournaments)
 #     return jsonify(results)
 
-print("Test job print.")
+
+# if __name__ == "__main__":
+#     update_database()
+
+from Tournaments import TournamentManagement
+from datetime import date, datetime
+from models import db, Tournament
+from flask import Flask
+
+app = Flask(__name__)
+
+# Configure your app with the appropriate settings
+app.config.from_object('config.ApplicationConfig')
+
+# Initialize the SQLAlchemy database
+db.init_app(app)
+
+def add_to_database(tournament):
+    date_format = "%Y-%m-%d"
+
+    if isinstance(tournament.date, str):
+        tournament.date = datetime.strptime(tournament.date, date_format)
+
+    found_tournament = (
+        db.session.query(Tournament)
+        .filter(
+            db.and_(
+                Tournament.name == tournament.name,
+                Tournament.areal == tournament.areal,
+                Tournament.date
+                == date(
+                    tournament.date.year, tournament.date.month, tournament.date.day
+                ),
+            )
+        )
+        .first()
+    )
+    
+    if found_tournament:
+        found_tournament.capacity = tournament.capacity
+        found_tournament.signed = tournament.signed
+    else:
+        db.session.add(tournament)
+    db.session.commit()
+
+def update_database():
+    tournaments = TournamentManagement()
+    found_tournaments = tournaments.run_all_scrapers()
+    for tournament in found_tournaments:
+        add_to_database(
+            Tournament(
+                name=tournament.name,
+                date=tournament.tournament_date,
+                city=tournament.city,
+                areal=tournament.areal,
+                capacity=tournament.capacity,
+                signed=tournament.signed,
+                price=tournament.price,
+                start=tournament.start,
+                organizer=tournament.organizer,
+                category=tournament.category,
+                level=tournament.level,
+                link=tournament.link,
+                user_id='1'
+            )
+        )
+
+if __name__ == "__main__":
+    with app.app_context():
+        update_database()
