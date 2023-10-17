a17cda30237283f0c3a3b3ffa17348b051d9c6d7
vacenric <vacenric@fit.cvut.cz>
2023-10-18 00:43:38 +0200

    Add try catch to driver init

--- Tournaments.py
+++ Tournaments.py
@@ -105,21 +105,21 @@ class TournamentManagement():
         return "Jiné"
 
     def open_chrome_with_url(self, url):
-        # try:
-        service = Service(executable_path=os.environ.get("CHROMEDRIVER_PATH"))
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--disable-dev-shm-usage")
-        chrome_options.add_argument("--no-sandbox")
-        self.driver = webdriver.Chrome(service=service, options=chrome_options)
-        self.driver.implicitly_wait(10)
-        self.driver.maximize_window()
-        self.driver.get(url)
-        return True
-        # except:
-        #     self.logger.error("Open chrom with url failed.")
-        #     return False
+        try:
+            service = Service(executable_path=os.environ.get("CHROMEDRIVER_PATH"))
+            chrome_options = webdriver.ChromeOptions()
+            chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+            chrome_options.add_argument("--headless")
+            chrome_options.add_argument("--disable-dev-shm-usage")
+            chrome_options.add_argument("--no-sandbox")
+            self.driver = webdriver.Chrome(service=service, options=chrome_options)
+            self.driver.implicitly_wait(10)
+            self.driver.maximize_window()
+            self.driver.get(url)
+            return True
+        except:
+            self.logger.error("Open chrom with url failed.")
+            return False
             
     def get_pbt_data(self,attempt):
         tournament_areal = "Prague Beach Team (Střešovice)"
