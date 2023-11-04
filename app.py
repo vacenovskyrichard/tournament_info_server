0f67e9b6675d23da0fb7fb5a69ff21789588c06c
vacenric <vacenric@fit.cvut.cz>
2023-11-04 17:27:09 +0100

    Add player and signed team to database and create api for it

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from datetime import date, datetime
 from datetime import datetime, timedelta, timezone
 from flask_bcrypt import Bcrypt
 from flask_mail import Mail, Message
-from models import db, Tournament, User, get_uuid
+from models import db, Tournament, User, Player,SignedTeam,get_uuid
 from config import ApplicationConfig
 from apscheduler.schedulers.background import BackgroundScheduler
 from ics import Calendar, Event
@@ -88,7 +88,12 @@ def refresh_expiring_jwts(response):
 def login():
     email = request.json.get("email", None)
     password = request.json.get("password", None)
-    user = User.query.filter_by(email=email).first()
+    isPlayer = request.json.get("isPlayer", None)
+    
+    if isPlayer:
+        user = Player.query.filter_by(email=email).first()
+    else:
+        user = User.query.filter_by(email=email).first()
 
     if user is None:
         return jsonify({"error": "Unauthorized"}), 401
@@ -105,11 +110,17 @@ def login():
 @app.route("/reset", methods=["POST"])
 def reset_password():
     email = request.json.get("email", None)
+    isPlayer = request.json.get("isPlayer", None)
     subject = 'Reset hesla'
     sender = 'jdem.hrat@email.cz'
     tmp_password = get_uuid()[-10:]
-    user = User.query.filter_by(email=email).first()
-
+    
+    
+    if isPlayer:
+        user = Player.query.filter_by(email=email).first()
+    else:
+        user = User.query.filter_by(email=email).first()
+    
     if user is None:
         return jsonify({"error": "User with this email does not exist"}), 404
     user.password =  bcrypt.generate_password_hash(tmp_password).decode("utf-8")
@@ -130,14 +141,23 @@ def register_user():
     password = request.json.get("password", None)
     name = request.json.get("name", None)
     surname = request.json.get("surname", None)
+    isPlayer = request.json.get("isPlayer", None)
 
-    user_exists = User.query.filter_by(email=email).first() is not None
 
+    if isPlayer:
+        user_exists = Player.query.filter_by(email=email).first() is not None
+    else:
+        user_exists = User.query.filter_by(email=email).first() is not None
+        
     if user_exists:
         return jsonify({"error": "User already exists"}), 409
     hashed_password = bcrypt.generate_password_hash(password).decode("utf-8")
-    new_user = User(email=email, password=hashed_password,name=name,surname=surname)
-
+    
+    if isPlayer:
+        new_user = Player(email=email, password=hashed_password,name=name,surname=surname)
+    else:
+        new_user = User(email=email, password=hashed_password,name=name,surname=surname)
+        
     db.session.add(new_user)
     db.session.commit()
 
@@ -149,11 +169,26 @@ def google_login():
     password = request.json.get("password", None)
     name = request.json.get("name", None)
     surname = request.json.get("surname", None)
+    isPlayer = request.json.get("isPlayer", None)
 
-    user_exists = User.query.filter_by(email=email).first() is not None
+    print("isPlayer")
+    print(isPlayer)
+    print(type(isPlayer))
+    
+
+    if isPlayer:
+
+        user_exists = Player.query.filter_by(email=email).first() is not None
+    else:
+        user_exists = User.query.filter_by(email=email).first() is not None
 
     if user_exists:
-        user = User.query.filter_by(email=email).first()
+        
+        if isPlayer:
+            user = Player.query.filter_by(email=email).first()
+        else:
+            user = User.query.filter_by(email=email).first()
+        
 
         if not bcrypt.check_password_hash(user.password, password):
             return jsonify({"error": "Unauthorized"}), 401
@@ -163,7 +198,13 @@ def google_login():
         return jsonify(response), 200    
 
     hashed_password = bcrypt.generate_password_hash(password).decode("utf-8")
-    new_user = User(email=email, password=hashed_password,name=name,surname=surname)
+        
+    if isPlayer:
+        new_user = Player(email=email, password=hashed_password,name=name,surname=surname)
+    else:
+        new_user = User(email=email, password=hashed_password,name=name,surname=surname)
+        
+    
     db.session.add(new_user)
     db.session.commit()
     
@@ -347,11 +388,18 @@ def filter_results():
 @jwt_required()
 def get_user_info():
     user_id = request.json.get("user_id", None)
-    user = User.query.filter_by(id=user_id).first()
+    isPlayer = request.json.get("isPlayer", None)
+    
+    if isPlayer:
+        user = Player.query.filter_by(id=user_id).first()
+        response = {"id":user.id,"name":user.name,"surname":user.surname,"email":user.email,"role": "player"}
+    else:
+        user = User.query.filter_by(id=user_id).first()
+        response = {"id":user.id,"name":user.name,"surname":user.surname,"email":user.email,"role":user.role}
     if not user:
         return jsonify({"error": "User not found."}), 404
-    
-    response = {"id":user.id,"name":user.name,"surname":user.surname,"email":user.email,"role":user.role}
+    print("response")
+    print(response)
     return jsonify(response), 200
 
 @app.route('/ical.feed/<city>/<areal>/<category>/<level>/', methods=["GET"])
@@ -433,6 +481,36 @@ def request_organizer_access():
     msg.body = f'Uživatel {user.name} {user.surname} žádá o práva organizátora.' 
     mail.send(msg)
     return "", 200
+
+
+@app.route("/create_team", methods=["POST"])
+def create_team():
+    player_id = request.json.get("player_id", None)
+    
+    tournament_id = request.json.get("tournament_id", None)
+    teammate_name = request.json.get("teammate_name", None)
+    teammate_surname = request.json.get("teammate_surname", None)
+    
+    new_team = SignedTeam(player_id=player_id,tournament_id=tournament_id,teammate_name=teammate_name,teammate_surname=teammate_surname)
+    db.session.add(new_team)
+    db.session.commit()
+
+    return jsonify({"message":"Team succesfully signed"}), 200
+
+@app.route("/delete_team", methods=["DELETE"])
+def delete_team():
+    player_id = request.json.get("player_id", None)
+    tournament_id = request.json.get("tournament_id", None)
+        
+    team_to_delete = SignedTeam.query.filter(SignedTeam.player_id == player_id,SignedTeam.tournament_id==tournament_id).first()
+    print("team_to_delete")
+    print(team_to_delete)
+    db.session.delete(team_to_delete)
+    db.session.commit()
+
+    return jsonify({"message":"Team succesfully deleted"}), 200
+
+
 
 if __name__ == "__main__":
     app.run()
